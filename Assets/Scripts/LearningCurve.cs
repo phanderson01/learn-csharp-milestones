c8aff341450bdc85f48bc6ede2ec6c5c3daed30b
phanderson01 <phanderson@davidson.edu>
2024-09-20 11:11:52 -0400

    compute age method

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,12 +6,16 @@ public class LearningCurve : MonoBehaviour
 {
 
     public int CurrentAge = 30;
+    public int AddedAge = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(30 + 1);
-        Debug.Log(CurrentAge + 1);
+        ComputeAge();
+    }
+    void ComputeAge()
+    {
+        Debug.Log(CurrentAge + AddedAge);
     }
 
     // Update is called once per frame
