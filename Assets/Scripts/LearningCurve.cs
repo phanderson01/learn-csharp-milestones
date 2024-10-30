891e5d43f13e6252c85b1750531a13edf8690fd5
phanderson01 <phanderson@davidson.edu>
2024-10-29 22:24:35 -0400

    test method update 6

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -41,7 +41,7 @@ public class LearningCurve : MonoBehaviour
     {
        /* Debug.LogFormat(0 + 1, number, secondnumber);*/
 
-        return (0 + 1)
+        return (0 + 1);
     }
 
     public void EnjoymentofClass()
